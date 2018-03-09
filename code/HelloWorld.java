f772e20b48f6ad1c9caca60952bf159b568e80dc
Markus Hiesmair <markus.hiesmair@yahoo.de>
2018-03-09 11:38:22 +0100

    added first listing

--- code/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-// this is a comment
-
-public class HelloWorld {
-
-	public static void main(String args[]) {
-		System.out.println("Hello World");
-	}
-
-}
---
+++ code/tensorflow_dense_neural_network.py
@@ -0,0 +1,24 @@
+def simple_dense_model(dataset, x_train, y_train):
+    hidden_dims = 64
+
+    model = Sequential()
+    model.add(Dense(hidden_dims, activation='relu', 
+	                input_dim=len(dataset.x[0])))
+    model.add(Dropout(0.2))
+    model.add(Dense(hidden_dims, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(hidden_dims, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(hidden_dims, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(hidden_dims, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(len(dataset.class_labels), 
+	                activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=200)
+    return model
