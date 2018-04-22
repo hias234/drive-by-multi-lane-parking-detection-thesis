fabfd450b61c7851cbb044047f3e1b00bcf55ce1
Markus Hiesmair <markus.hiesmair@yahoo.de>
2018-04-22 13:00:26 +0200

    included input from the corrections of KH

--- code/tensorflow_dense_neural_network.py
+++ code/tensorflow_dense_neural_network.py
@@ -1,6 +1,7 @@
 def simple_dense_model(dataset, x_train, y_train):
     hidden_dims = 64
 
+    # defining the deep neural network structure
     model = Sequential()
     model.add(Dense(hidden_dims, activation='relu', 
                     input_dim=len(dataset.x[0])))
@@ -16,9 +17,12 @@ def simple_dense_model(dataset, x_train, y_train):
     model.add(Dense(len(dataset.class_labels), 
                     activation='softmax'))
 
+    # setting optimization function and -metric
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
 
+    # training the model
     model.fit(x_train, y_train, epochs=200)
+	
     return model
